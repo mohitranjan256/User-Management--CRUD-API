0bf72b89dfb44f37b30ba23f535caf639dc68288
Mohit Ranjan <84243917+monster256@users.noreply.github.com>
2022-03-15 15:29:41 +0530

    Update user_update.js

--- routes/user_update.js
+++ routes/user_update.js
@@ -12,6 +12,8 @@ router.get('/update' , async(req,res) =>{
         const user_update = await user.updateOne({username : username}, {$set : { Name : Name ,password : password , phoneno : phoneno}})
 
         res.json(user_update);
+        console.log("User Data Updated..");
+        console.log(user_update);
     }
     catch(error){
         res.json({
